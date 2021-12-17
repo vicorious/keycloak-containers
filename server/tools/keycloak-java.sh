b8459bbaa4cde1d549d645782dfa39cebbf7a60b
Alejandro Lindarte Castro <alejo.lindarte@outlook.com>
2021-12-17 11:09:47 -0500

    Dockerfile for java keycloak

---
+++ server/tools/keycloak-java.sh
@@ -0,0 +1,5 @@
+###########################
+# Build/download Keycloak java #
+###########################
+git clone -b master https://github.com/vicorious/KeyCloackMobileAuthentication.git
+echo "Keycloak Java"
